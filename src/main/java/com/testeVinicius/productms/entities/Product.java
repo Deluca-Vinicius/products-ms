2ade6242b8180228662260dc778ed4c6a587438f
Deluca-Vinicius <vinidelucka@gmail.com>
2021-06-14 21:35:46 -0300

    Creating Product Entity

--- src/main/java/com/testeVinicius/productms/entities/Product.java
+++ src/main/java/com/testeVinicius/productms/entities/Product.java
@@ -1,6 +1,66 @@
 package com.testeVinicius.productms.entities;
 
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
 
+@Table
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private int price;
+
+    public Product(String id, String name, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
