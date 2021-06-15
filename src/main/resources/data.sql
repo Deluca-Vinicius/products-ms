925b73186ce257a9af9b27f0ef1d70d327ede476
Deluca-Vinicius <vinidelucka@gmail.com>
2021-06-15 19:41:54 -0300

    Configuration of memmory database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCT (id, description, name, price) VALUES ('1','Avocado juice', 'Avocado juice', 5);
+INSERT INTO PRODUCT (id, description, name, price) VALUES ('2','Orange juice', 'Orange juice', 3);
+INSERT INTO PRODUCT (id, description, name, price) VALUES ('3','Fruit vitamin', 'Fruit vitamin', 6);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE PRODUCT (
+                         ID VARCHAR(200) NOT NULL,
+                         DESCRIPTION VARCHAR(200) NOT NULL,
+                         NAME VARCHAR(200) NOT NULL,
+                         PRICE NUMBER NOT NULL
+);
