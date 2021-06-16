7ab29ea58d1cc566eb99a86cfa183bb4cca5f121
Deluca-Vinicius <vinidelucka@gmail.com>
2021-06-15 21:30:22 -0300

    Adjusting scripting to create H2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO PRODUCT (id, description, name, price) VALUES ('1','Avocado juice', 'Avocado juice', 5);
-INSERT INTO PRODUCT (id, description, name, price) VALUES ('2','Orange juice', 'Orange juice', 3);
-INSERT INTO PRODUCT (id, description, name, price) VALUES ('3','Fruit vitamin', 'Fruit vitamin', 6);
+INSERT INTO PRODUCT (id, description, name, price) VALUES ('3d86a6ad54af40628a163d60bc6c4383','Avocado juice', 'Avocado juice', 5);
+INSERT INTO PRODUCT (id, description, name, price) VALUES ('46996888aa84426fb34d8c41ada045d5','Orange juice', 'Orange juice', 3);
+INSERT INTO PRODUCT (id,  description, name, price) VALUES ('17b0234c6e114d129c6dbad6227af3c7','Fruit vitamin', 'Fruit vitamin', 6);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE PRODUCT (
-                         ID VARCHAR(200) NOT NULL,
+                         ID VARCHAR(200),
                          DESCRIPTION VARCHAR(200) NOT NULL,
                          NAME VARCHAR(200) NOT NULL,
                          PRICE NUMBER NOT NULL
